86c5a6c8dbbd14d167b0475047f9c67a7e00ecf4
Karan Singh <singh_karan@hotmail.com>
2022-12-09 19:04:38 +0530

    constants and name shadowing

--- rust_tutorial1/src/main.rs
+++ rust_tutorial1/src/main.rs
@@ -7,8 +7,9 @@ use std::fs::File;
 use rand::Rng;
 
 
-
-fn main() {
+fn greeting(){
+    /// Ask the user their name from CL
+    /// Returns a greeting 
     println!("What is your name?");
     let mut name = String::new();
     let greeting: &str = "Nice to meet you";
@@ -16,5 +17,21 @@ fn main() {
         .read_line(&mut name)
         .expect("Failed to readline");
     println!("Hello {}! {}", name.trim_end(), greeting);
+}
+
+fn constants_name_shadowing(){
+    const ONE_MIL: u32 = 1_000_000;
+    const PI: f32 = 3.141592;
+    let age: &str = "47";
+    // "Name shadowing" -> You can have 2 variables of different 
+    // types with the same name
+    let mut age: u32 = age.trim().parse()
+        .expect("age wasn't assigned a number");
+    age = age + 1;
+    println!("age:{}, mil: {}", age, ONE_MIL);
     
+}
+
+fn main() {
+    constants_name_shadowing()
 }
