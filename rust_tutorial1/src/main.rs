a4887454cfbcadc3656e299e40c38ae771f02537
Karan Singh <singh_karan@hotmail.com>
2022-12-09 18:48:00 +0530

    user input

--- rust_tutorial1/src/main.rs
+++ rust_tutorial1/src/main.rs
@@ -1,3 +1,20 @@
+
+#![allow(unused)] // Use this to allow imported crates(modules) that are not used 
+
+use std::io;
+use std::io::{Write, BufRead, BufReader, ErrorKind};
+use std::fs::File;
+use rand::Rng;
+
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("What is your name?");
+    let mut name = String::new();
+    let greeting: &str = "Nice to meet you";
+    io::stdin()
+        .read_line(&mut name)
+        .expect("Failed to readline");
+    println!("Hello {}! {}", name.trim_end(), greeting);
+    
 }
